3e241f9297c915fa1c92009d143f83e160a24b4c
“Rameshwar <rameshwar.banga@gmail.com>
2026-02-03 11:03:37 +0530

    added get api

--- controllers/myController.js
+++ controllers/myController.js
@@ -8,3 +8,11 @@ export const createTodo = async (req, res) => {
     return res.status(500).json({ msg: err.message });
   }
 };
+
+export const getAllTodos = async (req, res) => {
+  try {
+    const data = await MyTodoModel.find();
+    return res.status(200).json(data);
+  } catch (err) {}
+  return res.status(500).json({ mgs: err.message });
+};
--- routes/myRoute.js
+++ routes/myRoute.js
@@ -1,8 +1,10 @@
-import { createTodo } from "../controllers/myController.js";
+import { createTodo, getAllTodos } from "../controllers/myController.js";
+
 import express from "express";
 
 const router = express.Router();
 
 router.post("/", createTodo);
+router.get("/", getAllTodos);
 
 export default router;
